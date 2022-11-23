f27dff198c4fc1c70025a5c353802125f4f1881d
mabing <cumt_ttr@163.com>
2022-11-23 11:38:54 +0800

    for e2e init commit

--- examples/trigger/merge/curl.sh
+++
@@ -1,1 +0,0 @@
-curl -v  -H 'X-GitLab-Token: 1234567' -H 'X-Gitlab-Event: Merge Request Hook' -H 'Content-Type: application/json' --data-binary "@mergeevents.json" http://10.6.223.2:30661
--- examples/trigger/tagpush/curl.sh
+++
@@ -1,1 +0,0 @@
-curl -v  -H 'X-GitLab-Token: 1234567' -H 'X-Gitlab-Event: Tag Push Hook' -H 'Content-Type: application/json' --data-binary "@tag-pushevents.json" http://10.6.223.2:32718
